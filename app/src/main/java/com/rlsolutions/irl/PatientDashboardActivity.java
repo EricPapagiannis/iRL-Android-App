67500712f36163c0beeb3062e9a9fe3c4678754b
Eric Papagiannis <eric.papagiannis@mail.utoronto.ca>
2017-01-29 07:43:17 -0500

    added methods to increment and decrement

--- app/src/main/java/com/rlsolutions/irl/PatientDashboardActivity.java
+++ app/src/main/java/com/rlsolutions/irl/PatientDashboardActivity.java
@@ -84,7 +84,7 @@ public class PatientDashboardActivity extends AppCompatActivity {
         if (this.servingFru < this.totalServingFru) {
             this.servingFru++;
             TextView txt = (TextView) findViewById(R.id.servingsfruit);
-            txt.setText(servingFru + "/"  + totalServingFru);
+            txt.setText(this.servingFru + "/"  + this.totalServingFru);
             updateBar();
         } else {
             Toast.makeText(getApplicationContext(), "Please follow your diet!", Toast.LENGTH_SHORT).show();
@@ -101,4 +101,71 @@ public class PatientDashboardActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Invalid!", Toast.LENGTH_SHORT).show();
         }
     }
+
+    void addGra(View view) {
+        if (this.servingGra < this.totalServingGra) {
+            this.servingGra++;
+            TextView txt = (TextView) findViewById(R.id.servingsgrains);
+            txt.setText(this.servingGra + "/"  + this.totalServingGra);
+            updateBar();
+        } else {
+            Toast.makeText(getApplicationContext(), "Please follow your diet!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    void subGra(View view) {
+        if (this.servingGra> 0) {
+            this.servingGra--;
+            TextView txt = (TextView) findViewById(R.id.servingsgrains);
+            txt.setText(this.servingGra + "/"  + this.totalServingGra);
+            updateBar();
+        } else {
+            Toast.makeText(getApplicationContext(), "Invalid!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    void addMea(View view) {
+        if (this.servingMea < this.totalServingMea) {
+            this.servingMea++;
+            TextView txt = (TextView) findViewById(R.id.servingsmeat);
+            txt.setText(this.servingMea + "/"  + this.totalServingMea);
+            updateBar();
+        } else {
+            Toast.makeText(getApplicationContext(), "Please follow your diet!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    void subMea(View view) {
+        if (this.servingMea> 0) {
+            this.servingMea--;
+            TextView txt = (TextView) findViewById(R.id.servingsmeat);
+            txt.setText(this.servingMea + "/"  + this.totalServingMea);
+            updateBar();
+        } else {
+            Toast.makeText(getApplicationContext(), "Invalid!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    void addDai(View view) {
+        if (this.servingDai < this.totalServingDai) {
+            this.servingDai++;
+            TextView txt = (TextView) findViewById(R.id.servingsdairy);
+            txt.setText(this.servingDai + "/"  + this.totalServingDai);
+            updateBar();
+        } else {
+            Toast.makeText(getApplicationContext(), "Please follow your diet!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    void subDai(View view) {
+        if (this.servingDai> 0) {
+            this.servingDai--;
+            TextView txt = (TextView) findViewById(R.id.servingsdairy);
+            txt.setText(this.servingDai + "/"  + this.totalServingDai);
+            updateBar();
+        } else {
+            Toast.makeText(getApplicationContext(), "Invalid!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
