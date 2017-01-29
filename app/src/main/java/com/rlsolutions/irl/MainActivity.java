26294a997e5968e42ce4a375d858fc9186234a63
EricPapagiannis <eric.papagiannis@mail.utoronto.ca>
2017-01-29 02:59:48 -0500

    added better

--- app/src/main/java/com/rlsolutions/irl/MainActivity.java
+++ app/src/main/java/com/rlsolutions/irl/MainActivity.java
@@ -3,37 +3,54 @@ package com.rlsolutions.irl;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    NotificationManager notificationManager;
+    Button btn=(Button)findViewById(R.id.notif);
+    setContentView(R.layout.activity_main);
+    int notifID=33;
+    boolean isNotificActive=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      Button btn=(Button)findViewById(R.id.notif);
 
-        setContentView(R.layout.activity_main);
     }
     public void getNotification(View view)
     {
-        NotificationManager notificationmgr=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-        Intent intent=new Intent(this,PatientDashboardActivity.class);
-        PendingIntent pintent= PendingIntent.getActivities(this,(int) System.currentTimeMillis(), new Intent[]{intent}, 0);
-
-        Notification notif=new Notification.Builder(this)
+        NotificationCompat.Builder notif=new
+                NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.ic_stat_name)
-                .setContentTitle("INFORMATIONDKFJDSKFKL")
-                .setContentText("DJFDKFDSJKL")
+                .setContentTitle("Pls Hire Me")
+                .setContentText("I am GOOD MAN")
+                .setTicker("This is the message")
                 .setContentIntent(pintent)
                 .build();
-        notificationmgr.notify(0,notif);
+        Intent nextpage = new Intent(this, PatientDashboardActivity.class);
+        TaskStackBuilder tStackBuilder=TaskStackBuilder.create(this);
+        tStackBuilder.addParentStack(PatientDashboardActivity.class);
+        tStackBuilder.addNextIntent(nextpage);
+        PendingIntent pendingIntent=tStackBuilder.getPendingIntent(0,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        notif.setContentIntent(pendingIntent);
+        notificationManager =(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(notifID,notif.build());
+        isNotificActive=true;
+        
+
+
+
 
     }
+
 
     public void goToPatientDashboard(View view) {
         Intent intent = new Intent(this, PatientDashboardActivity.class);
