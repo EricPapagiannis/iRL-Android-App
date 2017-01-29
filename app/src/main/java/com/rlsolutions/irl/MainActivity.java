5c45fd6e3f1173131747fa4565c220a6558751ba
EricPapagiannis <eric.papagiannis@mail.utoronto.ca>
2017-01-28 23:58:40 -0500

    added notifications

--- app/src/main/java/com/rlsolutions/irl/MainActivity.java
+++ app/src/main/java/com/rlsolutions/irl/MainActivity.java
@@ -1,17 +1,38 @@
 package com.rlsolutions.irl;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      Button btn=(Button)findViewById(R.id.notif);
 
         setContentView(R.layout.activity_main);
+    }
+    public void getNotification(View view)
+    {
+        NotificationManager notificationmgr=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        Intent intent=new Intent(this,PatientDashboardActivity.class);
+        PendingIntent pintent= PendingIntent.getActivities(this,(int) System.currentTimeMillis(), new Intent[]{intent}, 0);
+
+        Notification notif=new Notification.Builder(this)
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentTitle("INFORMATIONDKFJDSKFKL")
+                .setContentText("DJFDKFDSJKL")
+                .setContentIntent(pintent)
+                .build();
+        notificationmgr.notify(0,notif);
+
     }
 
     public void goToPatientDashboard(View view) {
