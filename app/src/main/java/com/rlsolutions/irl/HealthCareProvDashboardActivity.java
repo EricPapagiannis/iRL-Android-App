e35fea95c0915469663d20782eda0806b2f89e16
Kareem Mohamed <kareem.mohamed@mail.utoronto.ca>
2017-01-28 19:57:01 -0500

    Fixed the second activity problem

--- app/src/main/java/com/rlsolutions/irl/HealthCareProvDashboardActivity.java
+++ app/src/main/java/com/rlsolutions/irl/HealthCareProvDashboardActivity.java
@@ -1,15 +1,13 @@
 package com.rlsolutions.irl;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class HealthCareProvDashboard extends AppCompatActivity {
+public class HealthCareProvDashboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, MainActivity.class);
         setContentView(R.layout.activity_health_care_prov_dashboard);
     }
 }
