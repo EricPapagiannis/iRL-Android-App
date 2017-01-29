02fc9086a9b520312fc1a05d4d5b81a30be7a29d
Eric Papagiannis <eric.papagiannis@mail.utoronto.ca>
2017-01-29 06:35:10 -0500

    adding servings and total servings

--- app/src/main/java/com/rlsolutions/irl/PatientDashboardActivity.java
+++ app/src/main/java/com/rlsolutions/irl/PatientDashboardActivity.java
@@ -4,10 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class PatientDashboardActivity extends AppCompatActivity {
 
     ProgressBar prg;
+    int servingVeg = 0;
+    int servingFru = 0;
+    int servingMea = 0;
+    int servingWhe = 0;
+    int servingDai = 0;
+    int totalServingVeg;
+    int totalServingFru;
+    int totalServingMea;
+    int totalServingWhe;
+    int totalServingDai;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +28,16 @@ public class PatientDashboardActivity extends AppCompatActivity {
         intent = new Intent(this, MainActivity.class);
         setContentView(R.layout.activity_patient_dashboard);
         prg = (ProgressBar) findViewById(R.id.progressBar);
+        this.totalServingVeg = 3;
+        this.totalServingFru = 2;
+        this.totalServingMea = 1;
+        this.totalServingWhe = 2;
+        this.totalServingDai = 2;
+        TextView txt = (TextView) findViewById(R.id.servingsvegetables);
+        txt.setText(servingVeg + "/"  + totalServingVeg);
+        txt.setText(servingFru + "/"  + totalServingFru);
+        txt.setText(servingMea + "/"  + totalServingMea);
+        txt.setText(servingWhe + "/"  + totalServingWhe);
+        txt.setText(servingDai + "/"  + totalServingDai);
     }
 }
